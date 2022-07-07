348745d8ad6485aed722a9ede7519e3be6136a09
asands94 <ashley.sands01@gmail.com>
2022-07-07 14:16:10 -0500

    wip: hide secret key

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c8jj$gv-8j3a9zibjfu583bqkfwc2)xir=^5x!6e=q^n$y)jq8'
+
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
