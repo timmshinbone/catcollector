f42f0ce82047e5e3486a0919baac06a3442cff17
asands94 <ashley.sands01@gmail.com>
2022-07-08 10:59:07 -0500

    config

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -25,7 +25,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
