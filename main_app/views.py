653945b99827330b3b909e4699c6a80c2cb7ccb3
asands94 <ashley.sands01@gmail.com>
2022-07-08 09:24:41 -0500

    wip: fix typo

--- main_app/views.py
+++ main_app/views.py
@@ -83,7 +83,7 @@ def add_photo(request, cat_id):
 
 def signup(request):
   error_message = ''
-  if request.methof == "POST":
+  if request.method == "POST":
     # This is how to create a 'user' form object that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
