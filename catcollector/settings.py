f35f3cc14f26e201e8b156e6520dc6e28cef11e6
asands94 <ashley.sands01@gmail.com>
2022-07-08 09:13:39 -0500

    wip: add logout redirect

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/cats/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
