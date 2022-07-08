4db706362417fbf28e27aa936d51f3af1112fc7a
asands94 <ashley.sands01@gmail.com>
2022-07-08 09:16:55 -0500

    wip: assign user to created cat

--- main_app/views.py
+++ main_app/views.py
@@ -85,6 +85,14 @@ class CatCreate(CreateView):
   fields = ['name', 'breed', 'description', 'age']
   success_url = '/cats/'
 
+  # This inherited method is called when a
+  # valid cat form is being submitted
+  def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+    return super().form_valid(form)
+
 class CatUpdate(UpdateView):
   model = Cat
   # Let's disallow the renaming of a cat by excluding the name field!
