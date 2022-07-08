42cfa675d8bb5b3e22530ef36d88d9d430300aa1
asands94 <ashley.sands01@gmail.com>
2022-07-08 10:06:51 -0500

    wip: add a users profile information to the admin view

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # import your models here
-from .models import Cat, Feeding, Toy, Photo
+from .models import Cat, Feeding, Toy, Photo, Profile, User
 
 # Register your models here
 admin.site.register(Cat)
@@ -10,3 +11,19 @@ admin.site.register(Feeding)
 admin.site.register(Toy)
 
 admin.site.register(Photo)
+
+
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+  model = Profile
+  can_delete = False
+  verbose_name_plural = 'profile'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+  inlines = (ProfileInline),
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
