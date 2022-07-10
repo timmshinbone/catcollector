6fc57406eb36568faf9a1137e602e3ffe7aa73e0
asands94 <ashley.sands01@gmail.com>
2022-07-10 17:35:51 -0500

    wip: signup form edit

--- main_app/views.py
+++ main_app/views.py
@@ -110,6 +110,7 @@ def signup(request):
   # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
+  
   return render(request, 'registration/signup.html', context)   
 
 class CatCreate(LoginRequiredMixin, CreateView):
